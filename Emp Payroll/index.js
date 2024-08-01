aa01ad12ec1f611f097694dee602f0165a4b6f69
Sneha Shrinivas <Sneha.Shrinivas@godigit.com>
2024-08-01 17:31:15 +0530

    Added: Employee Payroll codes

---
+++ Emp Payroll/index.js
@@ -0,0 +1,77 @@
+$(function (e) {
+    // alert("hi")
+    getEmployeeDetails();
+});
+ 
+function getEmployeeDetails() {
+    // alert("hello");
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/Employee",
+        success: function (res) {
+            // let data = "";
+            console.log(res);
+            for (var i = 0; i < res.length; i++) {
+                let data = `
+            <tr>
+                <td class="table_val_name"><img src="../Assets/${res[i].url}" height="32px" width="30px" style="border-radius :15px"></td>
+                <td><span class="span_name">${res[i].name}</span></td>
+                <td class="table_val">${res[i].gender}</td>
+                <td class="table_val"><span style="background-color: #E9FEA5;">${res[i].department}</span></td>
+                <td class="table_val">${res[i].salary}</td>
+                <td class="table_val">${res[i].startDate}</td>
+                <td class="table_val"><button style="background-color: white; border:none" id="delete" onclick="deleterow('${res[i].id}')"><img style="background-color: white" src="../Assets/delete-black-18dp.svg"></button>&nbsp;&nbsp;&nbsp;&nbsp;<button style="background-color: white; border:none" id="edit" onclick="edit()"><img style="background-color: white" src="../Assets/create-black-18dp.svg"></button></td>
+            </tr>
+                `;
+                $("#tbl-details").append(data);
+            }
+        },
+        error: function (err) {
+ 
+        }
+    })
+}
+ 
+function SearchBar() {
+    var search = document.getElementById("searchInput").value;
+    search = search.toLowerCase();
+ 
+    var table, tr, td, txtvalue, index;
+ 
+    tr = document.getElementsByTagName("tr");
+    // it takes all tr including heading... heading in tr[0]. so we started the loop from 1
+ 
+    for (var i = 1; i < tr.length; i++) {
+        td = tr[i].children;
+        let tr_has = false;
+        for (var j = 0; j < td.length; j++) {
+            var newtd = td[j];
+            if (newtd) {
+                txtvalue = newtd.innerText;
+                if (txtvalue.toLowerCase().includes(search)){
+                    tr_has = true;
+                }
+            }
+            tr[i].style.display = (tr_has) ? "" : "none";
+        }
+    }
+};
+ 
+//delete row data
+function deleterow(id) {
+    $.ajax({
+        url: `http://localhost:3000/Employee/`+id,
+        type: 'DELETE',
+        success: function(data) {
+            console.log(data.message);
+            location.reload(); // Reload the table data
+        },
+        error: function() {
+        }
+    });
+}
+ 
+//edit
+function edit(){
+ 
+}   
---
+++ Payroll Form/form.js
@@ -0,0 +1,95 @@
+const daySelect = document.getElementById('inputDay');
+for (let i = 1; i <= 31; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = i;
+    daySelect.appendChild(option);
+}
+ 
+// Populate months
+const monthSelect = document.getElementById('inputMonth');
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+monthNames.forEach((month, index) => {
+    const option = document.createElement('option');
+    option.value = index + 1;
+    option.textContent = month;
+    monthSelect.appendChild(option);
+});
+ 
+// Populate years
+const yearSelect = document.getElementById('inputYear');
+const currentYear = new Date().getFullYear();
+for (let i = currentYear; i >= 1900; i--) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = i;
+    yearSelect.appendChild(option);
+}
+ 
+ 
+// form data submit
+function submitbtn() {
+   
+    //photo
+    let profileimg = document.querySelector('input[name="profile-image"]:checked').value;
+    // if (profileimg) {
+    //     console.log(profileimg.value);
+    // } else {
+    //     console.log("no img selected");
+    // }
+ 
+    //name
+    let fname = document.querySelector("#inputName").value;
+    // console.log(fname);
+ 
+    //Gender
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+    // console.log(gender);
+ 
+    //Department
+    let dept = document.querySelector('input[name="dept"]:checked').value;
+    // console.log(dept);
+ 
+    //Salary
+    let salary = document.querySelector('#inputState').value;
+    // console.log(salary);
+ 
+    //Start Date
+    let date = $('#inputDay').val() + '/' + $('#inputMonth').val() + '/' + $('#inputYear').val();
+    // console.log(date);
+ 
+    //user
+   
+    const user = {
+        url: profileimg,
+        name: fname,
+        gender: gender,
+        department: dept,
+        salary: salary,
+        startDate: date
+ 
+    }
+ 
+    $.ajax({
+        url: 'http://localhost:3000/Employee',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(user),
+        success: function (data) {
+            console.log('User added:', data);
+            window.opener.location.reload();
+        },
+        error: function (error) {
+            console.error('Error:', error);
+        }
+    });
+    window.location.replace("../Emp Payroll/index.html");
+}
+
+function cancelbtn(){
+    window.location.replace("index.html");
+}
+
+function restbtn(){
+    window.location.replace("form.html");
+}
